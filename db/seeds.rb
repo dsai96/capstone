1e19743f0d9aee3543cdd16fe3836ee56fa56477
Divya <drmohan@andrew.cmu.edu>
2017-11-07 20:02:37 -0500

    seeding pt 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,34 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  # Major.create(name: 'Information Systems')
 
-# contents = File.open("snippet.txt", "r"){ |file| file.read }
 
 require 'rubygems'
 require 'json/pure'
+
+def createMajors
+	Major.create(name: 'Information Systems')
+end
+
+def createRequirements
+	requirements = [
+		["Mathematics and Statistics 1", "C", 28],
+		["Mathematics and Statistics 2", "C", 28],
+		["Mathematics and Statistics 3", "C", 28],
+		["Computer Science", "C", 32],
+		["Professional Core", "C", 51],
+		["Professional Core Elective", "C", 6],
+		["Professional Communications", "C", 9],
+		["Quantitative Analysis and Research Methods", "C", 9],
+		["Organizations, Policy, and Social Science", "C", 9],
+		["Business/Enterprise Systems", "C", 27],
+		["Computing and Information Systems & Technology", "C", 27],
+		["Social and Global Systems", "C", 27],
+		["Quantitative Analysis", "C", 27],
+		["IDeATe", "C", 27],
+
+	]
+end
 
 contents = File.open("db/out.csv", "r"){ 
 	|file| file.read 
